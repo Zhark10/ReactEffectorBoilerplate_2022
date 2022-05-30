5fd75a65e47c83a69ce79325e84db9fba445e22a
Zhark10 <verygoodman18@yandex.ru>
2022-05-31 01:02:32 +0300

    profile logout

--- src/view/pages/private/ProfilePage/index.tsx
+++ src/view/pages/private/ProfilePage/index.tsx
@@ -1,9 +1,17 @@
+import React from 'react';
+import { EButtonType, UIButton } from '../../../components/atoms/UIButton/UIButton';
 import PageContainer from '../../../components/molecules/PageContainer/PageContainer';
+import { useViewModel } from './view-model';
 
 export default function ProfilePage() {
+  const { methods: { logout }, data: { profile } } = useViewModel()
+
+  if (!profile.user) return <React.Fragment/>
+
   return (
     <PageContainer>
-      Профиль
+      {profile.user.firstName}
+      <UIButton onClick={logout} text="Выйти" type={EButtonType.CONTAINED} />
     </PageContainer>
   );
 }
--- src/view/pages/private/ProfilePage/view-model.ts
+++ src/view/pages/private/ProfilePage/view-model.ts
@@ -1,15 +1,26 @@
 
+import { AuthEntity } from "../../../../stores/auth/entity";
 import { ModalEntity } from "../../../../stores/modal/entity";
+import { ProfileEntity } from "../../../../stores/profile/entity";
 
 export const useViewModel = () => {
+  const profile = ProfileEntity.selectors.useProfile()
+
   const showProfileCRUDModal = () => {
     ModalEntity.events.add({ name: "ModalProfile", toNextModalProps: {} })
   }
 
+  const logout = () => {
+    AuthEntity.events.logoutClicked()
+  }
+
   return {
     methods: {
-      showProfileCRUDModal
+      showProfileCRUDModal,
+      logout,
     },
-    data: {}
+    data: {
+      profile
+    }
   }
 }
