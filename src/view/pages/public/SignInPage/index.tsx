c67f4f76decdafd7c3ec0669a4fb0c3d306f2482
Zhark10 <verygoodman18@yandex.ru>
2022-05-31 01:02:13 +0300

    sign in page styles

--- src/view/pages/public/SignInPage/index.tsx
+++ src/view/pages/public/SignInPage/index.tsx
@@ -1,15 +1,11 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
+import TextField, { TextFieldProps } from '@mui/material/TextField';
 import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import {createTheme, ThemeProvider} from '@mui/material/styles';
-import {useViewModel} from './view-model';
-import {Controller} from 'react-hook-form';
+import { createTheme } from '@mui/material/styles';
+import { useViewModel } from './view-model';
+import { Controller } from 'react-hook-form';
+import { EButtonType, UIButton } from '../../../components/atoms/UIButton/UIButton';
 
 const theme = createTheme();
 
@@ -17,76 +13,64 @@ export default function SignInPage() {
   const { methods, data: { control } } = useViewModel()
 
   return (
-    <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'primary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Авторизация
-          </Typography>
-          <Box component="form" onSubmit={methods.submit} noValidate sx={{ mt: 1 }}>
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Typography color="primary" component="h1" variant="h5">
+          Вход
+        </Typography>
+        <Box component="form" onSubmit={methods.submit} noValidate sx={{ mt: 1 }}>
 
-            <Controller
-              name="login"
-              control={control}
-              defaultValue=""
-              render={({ field: { onChange, value }, fieldState: { error } }) => (
-                <TextField
-                  margin="normal"
-                  fullWidth
-                  label="Логин"
-                  variant="filled"
-                  value={value}
-                  onChange={onChange}
-                  error={!!error}
-                  autoFocus
-                  helperText={error ? error.message : null}
-                  type="text"
-                />
-              )}
-              rules={{ required: 'Необходимо заполнить логин' }}
-            />
+          <Controller
+            name="login"
+            control={control}
+            defaultValue=""
+            render={({ field: { onChange, value }, fieldState: { error } }) => (
+              <TextField
+                {...commonInputProps(onChange, error)}
+                label="Логин"
+                value={value}
+                type="text"
+              />
+            )}
+            rules={{ required: 'Необходимо заполнить логин' }}
+          />
 
-            <Controller
-              name="password"
-              control={control}
-              defaultValue=""
-              render={({ field: { onChange, value }, fieldState: { error } }) => (
-                <TextField
-                  margin="normal"
-                  fullWidth
-                  label="Пароль"
-                  variant="filled"
-                  value={value}
-                  onChange={onChange}
-                  error={!!error}
-                  helperText={error ? error.message : null}
-                  type="password"
-                />
-              )}
-              rules={{ required: 'Необходимо заполнить пароль' }}
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Войти
-            </Button>
-          </Box>
+          <Controller
+            name="password"
+            control={control}
+            defaultValue=""
+            render={({ field: { onChange, value }, fieldState: { error } }) => (
+              <TextField
+                {...commonInputProps(onChange, error)}
+                label="Пароль"
+                value={value}
+                type="password"
+              />
+            )}
+            rules={{ required: 'Необходимо заполнить пароль' }}
+          />
+
+          <UIButton fullWidth onClick={methods.submit} text="Войти" type={EButtonType.CONTAINED} />
         </Box>
-      </Container>
-    </ThemeProvider>
+      </Box>
+    </Container>
   );
+}
+
+function commonInputProps(onChange: (...event: any[]) => void, error: any): TextFieldProps {
+  return {
+    fullWidth: true,
+    onChange,
+    error: Boolean(error),
+    autoFocus: true,
+    helperText: error ? error.message : null,
+    style: { marginBottom: 12 }
+  } as TextFieldProps
 }
